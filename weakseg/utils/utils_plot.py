11480ca8249df8728dc9c6a0deea440f6fa8a421
Francesco Fontan <francesco.fontan@gmail.com>
2021-07-10 18:23:25 +0200

    fefactor visualize

--- weakseg/utils/utils_plot.py
+++ weakseg/utils/utils_plot.py
@@ -1,10 +1,11 @@
 
+from typing import Dict
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def visualize(**images):
-    """PLot images in one row."""
+def visualize(images: Dict[str, np.ndarray], save_flag=False, filepath_fig='img.png'):
+    """Plot images in one row."""
     n = len(images)
     plt.figure(figsize=(16, 5))
     for i, (name, image) in enumerate(images.items()):
@@ -14,3 +15,6 @@ def visualize(**images):
         plt.title(' '.join(name.split('_')).title())
         plt.imshow(image)
     plt.show()
+
+    if save_flag:
+        plt.savefig(filepath_fig)
